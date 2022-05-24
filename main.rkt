a6e4d7dce5ad3e8d094809013f679e8965ddde95
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2022-05-23 20:21:02 -0700

    test

--- main.rkt
+++ main.rkt
@@ -1,14 +1,8 @@
 #lang racket
 
-5177441341407232/1073741824
-
-(/ 5177441341407232 1073741824)
-
-(define cnt 0)
-(for ([i 1000000])
-  #:break (= cnt 5)
-  (define prgen (make-pseudo-random-generator))
-  (define init-pregen-vec (pseudo-random-generator->vector prgen))
-  (when (for/or ([x init-pregen-vec]) (not (integer? x)))
-    (set! cnt (add1 cnt))
-    (println (list i init-pregen-vec))))
+(for ([i (in-range 2 35)])
+  (for ([j 100])
+    (define a (* (expt 2 i) (random 10000000)))
+    (define b (expt 2 i))
+    (println (list a b (/ a b))))
+  (newline))
